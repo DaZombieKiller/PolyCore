2cd8f4ad391055fb31a08d9312721b323cfd334a
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2025-06-18 19:32:43 +1000

    Add Enumerable.Reverse T[] overload

---
+++ PolyCore/System.Linq/Enumerable.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace System.Linq
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class _Enumerable
+    {
+        public static IEnumerable<TSource> Reverse<TSource>(this TSource[] source) => Enumerable.Reverse(source);
+    }
+}
+
+namespace @implicit
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class _System_Linq_Enumerable
+    {
+        extension(Enumerable)
+        {
+            public static IEnumerable<TSource> Reverse<TSource>(TSource[] source) => Enumerable.Reverse(source);
+        }
+    }
+}
